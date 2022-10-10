ad5e3896ded9cc1ca0f56d1169f949604941d7db
kindheart-is-good <oneofmycontacts5@gmail.com>
2022-10-10 17:48:17 +0300

    Add Preloader and isFetching indication in Users component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {followAC, unfollowAC, setUsersAC, setCurrentPageAC, setUsersTotalCountAC} from "../../redux/users-reducer";
+import {
+    followAC,
+    unfollowAC,
+    setUsersAC,
+    setCurrentPageAC,
+    setUsersTotalCountAC,
+    toggleIsFetchingAC
+} from "../../redux/users-reducer";
 import * as axios from "axios";
 import Users from "./Users";
-import preloader from '../../assets/images/preloader.svg';
+import Preloader from "../common/Preloader/Preloader";
 
 /* В этом файле у нас сразу 2 контейнерных компоненты, вложенных друг в друга.
 * Задача UsersContainer выполнять GET-запросы.
@@ -24,9 +31,11 @@ class UsersContainer extends React.Component {
                     location: {city: 'Kiev', country: 'Ukraine'} },]
             )*/
 
+        this.props.toggleIsFetching(true);
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
                 //debugger;
+                this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
                 this.props.setTotalUsersCount(response.data.totalCount);
                 //debugger;
@@ -38,25 +47,30 @@ class UsersContainer extends React.Component {
         /* верхней строчкой мы диспатчим в store новое значение.
         Затем store вернёт нам значение через props, но сначала доработает данная функция (всё тело, ниже). */
 
+        this.props.toggleIsFetching(true);
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
                 //debugger;
+                this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
                 //debugger;
             });
     }
 
     render() {
-        return <Users totalUsersCount={this.props.totalUsersCount}
-                      pageSize={this.props.pageSize}
-                      currentPage={this.props.currentPage}
-                      /*onPageChanged={this.onPageChanged}*/
-                      onPageChanged={this.onPageChanged.bind(this)}
-                      /*onPageChanged={() => {this.onPageChanged}}*/
-                      users={this.props.users}
-                      follow={this.props.follow}
-                      unfollow={this.props.unfollow}
-        />
+        return <>
+            { this.props.isFetching ? <Preloader /> : null }
+            <Users totalUsersCount={this.props.totalUsersCount}
+                   pageSize={this.props.pageSize}
+                   currentPage={this.props.currentPage}
+                   /*onPageChanged={this.onPageChanged}*/
+                   onPageChanged={this.onPageChanged.bind(this)}
+                   /*onPageChanged={() => {this.onPageChanged}}*/
+                   users={this.props.users}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow}
+            />
+            </>
     }
 }
 
@@ -86,6 +100,9 @@ let mapDispatchToProps = (dispatch) => {
         },
         setTotalUsersCount: (totalCount) => {
             dispatch(setUsersTotalCountAC(totalCount));
+        },
+        toggleIsFetching: (isFetching) => {
+            dispatch(toggleIsFetchingAC(isFetching));
         },
     }
 }
